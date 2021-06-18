369d5744eb085e163354765110f932fb534b55a4
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2021-06-18 11:09:03 -0500

    add some cors config

--- restdjango/myrestapi/myrestapi/settings.py
+++ restdjango/myrestapi/myrestapi/settings.py
@@ -142,5 +142,27 @@ CORS_ALLOWED_ORIGINS = [
     #"https://example.com",
     #"https://sub.example.com",
     "http://localhost:8000",
-    #"http://127.0.0.1:9000"
+    "http://127.0.0.1:8000"
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
 ]
